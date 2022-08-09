57af8e5c41a05e216190529c8b8182e79ba85502
tingtt <ting.taku@gmail.com>
2022-08-10 01:58:28 +0900

    style: use tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Fira Sans',
+        'Droid Sans',
+        'Helvetica Neue',
+        'sans-serif',
+      ],
+    },
   },
   plugins: [],
 }
